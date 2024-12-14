1ff1fbbcadb0cc10cc05141fed4347af9851def9
Ali Yar Khan <aliyark145@gmail.com>
2024-12-15 00:14:54 +0500

    first commit

---
+++ src/app/font.ts
@@ -0,0 +1,7 @@
+import { Josefin_Sans } from 'next/font/google'
+//SF Pro Display
+
+export const josefinSans = Josefin_Sans({
+  variable: '--font-josefin-sans',
+  weight: ['700']
+})
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,122 @@
-import Image from "next/image";
+import Image from 'next/image'
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle
+} from '@/components/ui/navigation-menu'
+import { josefinSans } from './font'
+import { Button, buttonVariants } from '@/components/ui/button'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className='min-w-max min-h-screen'>
+      <header className='w-full flex flex-row justify-between px-9 py-8'>
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src={'/logo.png'}
+          width={100}
+          height={52.69}
+          alt='wandering-pro'
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuLink className='font-cabin font-semibold text-[20px] leading-[39.4px] text-[#150306]'>
+                JOIN DISCORD
+              </NavigationMenuLink>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className='font-cabin font-semibold text-[20px] leading-[39.4px] text-[#150306]'>
+                CONTENT
+              </NavigationMenuTrigger>
+              <NavigationMenuContent></NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className='font-cabin font-semibold text-[20px] leading-[39.4px] text-[#150306]'>
+                COMMUNITY
+              </NavigationMenuTrigger>
+              <NavigationMenuContent></NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className='font-cabin font-semibold text-[20px] leading-[39.4px] text-[#150306]'>
+                RESOURCES
+              </NavigationMenuTrigger>
+              <NavigationMenuContent></NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className='font-cabin font-semibold text-[20px] leading-[39.4px] text-[#150306]'>
+                MORE
+              </NavigationMenuTrigger>
+              <NavigationMenuContent></NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Image
+                src={'/search.png'}
+                width={18}
+                height={18.01}
+                alt='search'
+              />
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </header>
+      <main className='flex flex-col'>
+        <section className='w-full flex flex-row justify-between mt-[15px] pl-[50px]'>
+          <div className='min-w-1/2'>
+            <div className='flex flex-row max-w-[648px]'>
+              <h1
+                className={`${josefinSans.variable} font-bold text-[80px] leading-[96px] text-[#2C2C2C]`}
+              >
+                THE WANDERING PRO
+              </h1>
+            </div>
+            <div className='flex flex-row'>
+              <span
+                className={`${josefinSans.variable} font-bold text-[24px] leading-[28.64px] text-[#31B6C0]`}
+              >
+                Mentorship Space For Builders
+              </span>
+            </div>
+            <div className='flex flex-row max-w-[552px] mt-6'>
+              <span
+                className={`${josefinSans.variable} font-normal text-[24px] leading-[28.64px] text-[#2C2C2C]`}
+              >
+                Pakistan’s first community for building Products, Projects, and
+                Portfolios. Join our Discord for mentorship, project support,
+                and collaboration to grow your career!
+                <br />
+                <br />
+                <span className='italic font-semibold'>
+                  Hosted by: Saqib Tahir
+                </span>
+              </span>
+            </div>
+            <div className='flex flex-row mt-6'>
+              <Link
+                href={''}
+                className={`${buttonVariants({
+                  variant: 'outline'
+                })} w-[216px] h-[60px] rounded-2xl leading-[28.64px] bg-[#31B6C0] hover:bg-[#31B6C0] hover:text-[#2C2C2C] text-[#2C2C2C]`}
+              >
+                <span className='font-bold text-[20px]'>Join Discord</span>
+              </Link>
+            </div>
+          </div>
+          <div className='min-w-1/2 w-[734px] h-[727px]'>
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={'/hero-images.png'}
+              width={634}
+              height={627}
+              alt='hero-images'
+              className='shad'
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
-  );
+  )
 }
