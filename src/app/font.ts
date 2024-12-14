f2a1c44b2844c3a341dbc62c517018319300c2e3
Ali Yar Khan <aliyark145@gmail.com>
2024-12-15 00:21:56 +0500

    subset set for josefinSans font

--- src/app/font.ts
+++ src/app/font.ts
@@ -2,6 +2,7 @@ import { Josefin_Sans } from 'next/font/google'
 //SF Pro Display
 
 export const josefinSans = Josefin_Sans({
+  subsets: ['latin'],
   variable: '--font-josefin-sans',
   weight: ['700']
 })
